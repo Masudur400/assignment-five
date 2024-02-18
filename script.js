5275ac2820a73b73ce192a9841025bc6c3869023
Masudur400 <md8004210@gmail.com>
2024-02-18 16:30:50 +0600

    last update

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
- function ticketSection(){
-    
-   scrollTo(document.getElementById('ticket-section'));
+
+const allBtn = document.querySelectorAll('#btn');
+let count = 0;
+for (const btn of allBtn) {
+  btn.addEventListener('click', function (e) {
+    btn.style.backgroundColor = 'lightGreen'; 
+    count = count + 1;
+
+
+    //  document.getElementById('total-cost').innerText = convertedTotalCost + cost;
+
+    sitLeft ()
+    totalCost();
+    grandTotalCost();
+    setInnerText('sit-count', count);
+  })
+}
+
+
+function totalCost() {
+  const totalCost = document.getElementById('total-cost').innerText;
+  const convertedTotalCost = parseInt(totalCost);
+  const cost = convertedTotalCost + 550; 
+  setInnerText('total-cost', cost)
+}
+
+function grandTotalCost() {
+  const totalCost = document.getElementById('total-cost').innerText; 
+  setInnerText('grand-total', totalCost)
+}
+ 
+function sitLeft (){
+  const sit = document.getElementById('sit-left').innerText;
+   const convertedSit = parseInt(sit);
+   sum = convertedSit - 1;
+   setInnerText('sit-left',sum);
+}
+
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
+
+
+
+
+function next() {
+  const homeSection = document.getElementById('header-section');
+  homeSection.classList.add('hidden');
+
+  const successSection = document.getElementById('success');
+  successSection.classList.remove('hidden');
 }
